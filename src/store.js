98178619dfb62eb8b9339d63deab576919e748d7
Hu Chen <chen@huchen.me>
2019-02-09 20:17:24 +0800

    feat: Prepare for creating extension

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { combineReducers } from 'redux';
 import github from './redux/github';
@@ -8,13 +8,5 @@ const rootReducer = combineReducers({
 });
 
 export default function configureStore(preloadedState) {
-  return createStore(
-    rootReducer,
-    preloadedState,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 }
