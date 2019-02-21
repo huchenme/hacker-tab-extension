872f66baab8413ffad8fd816ff82cfd1c2811656
Hu Chen <chen.hu@sc.com>
2019-02-21 11:57:41 +0800

    fix(ga): Fix Google Analytics

---
+++ public/ga.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+// Reference: https://davidsimpson.me/2014/05/27/add-googles-universal-analytics-tracking-chrome-extension/
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  (i[r] =
+    i[r] ||
+    function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(
+  window,
+  document,
+  'script',
+  'https://www.google-analytics.com/analytics.js',
+  'ga'
+); // Note: https protocol here
+
+ga('create', 'UA-134959994-1', 'auto');
+ga('set', 'checkProtocolTask', function() {}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+ga('require', 'displayfeatures');
+ga('send', 'pageview', '/index.html'); // Specify the virtual path
