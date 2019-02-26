be707ceb262a6f285a2cc05c0ac3b7e81a2c71ca
Hu Chen <chen.hu@sc.com>
2019-02-26 16:19:51 +0800

    chore: Add back redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { combineReducers } from 'redux';
 import github from './redux/github';
 
-const rootReducer = combineReducers({
-  github,
-});
+const rootReducer = combineReducers({ github });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(preloadedState) {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(thunk))
+  );
 }
