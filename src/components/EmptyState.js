d229969ee3146cef6d242a9a861899281021b6d5
Hu Chen <chen@huchen.me>
2019-06-24 22:02:34 +0800

    chore: refactor components to use emotion css instead of styled

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import styled from '@emotion/styled';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 
 import { ReactComponent as EmptyIcon } from '../images/empty.svg';
 
 export default function InfoItem({ children, icon }) {
   return (
-    <Container>
-      <StyledEmptyIcon height={32} width={32} />
+    <div
+      css={css`
+        margin: auto;
+        background-color: #fafbfc;
+        border: 1px solid #e1e4e8;
+        border-radius: 3px;
+        box-shadow: inset 0 0 10px rgba(27, 31, 35, 0.05);
+        padding: 32px;
+        padding-bottom: 48px;
+        text-align: center;
+        max-width: 900px;
+      `}
+    >
+      <EmptyIcon
+        css={css`
+          color: #a3aab1;
+          fill: currentColor;
+          margin-left: 4px;
+          margin-right: 4px;
+        `}
+        height={32}
+        width={32}
+      />
       <h3>
         Trending repositories results are currently being dissected in{' '}
         <a href="https://github.com/trending">GitHub</a>.
@@ -15,25 +36,6 @@ export default function InfoItem({ children, icon }) {
         This may be a few minutes. Now would be a great time to write that novel
         you have always been talking about.
       </p>
-    </Container>
+    </div>
   );
 }
-
-const Container = styled.div`
-  margin: auto;
-  background-color: #fafbfc;
-  border: 1px solid #e1e4e8;
-  border-radius: 3px;
-  box-shadow: inset 0 0 10px rgba(27, 31, 35, 0.05);
-  padding: 32px;
-  padding-bottom: 48px;
-  text-align: center;
-  max-width: 900px;
-`;
-
-const StyledEmptyIcon = styled(EmptyIcon)`
-  color: #a3aab1;
-  fill: currentColor;
-  margin-left: 4px;
-  margin-right: 4px;
-`;
