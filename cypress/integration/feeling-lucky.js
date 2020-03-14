83aee5b402b9ee271a0f79bbf63fa72c1f562b50
Hu Chen <chen@huchen.me>
2020-03-14 14:35:49 +0800

    add test for feeling lucky

--- cypress/integration/feeling-lucky.js
+++ cypress/integration/feeling-lucky.js
@@ -1,13 +1,52 @@
 describe('I’m Feeling Lucky', () => {
   it('should have a lucky repo', () => {
     cy.fetchReposAndWait();
-    cy.findByText('I’m Feeling Lucky').should('exist');
-    cy.findByTestId('random-repo-list')
-      .findAllByTestId('repo-card')
-      .should('have.length', 1);
-    cy.findByLabelText('Random Pick Button').click();
-    cy.findByTestId('random-repo-list')
-      .findAllByTestId('repo-card')
-      .should('have.length', 1);
+
+    cy.fixture('trending').then(json => {
+      const names = json.map(repo => repo.name);
+      cy.findByText('I’m Feeling Lucky').should('exist');
+      cy.findByTestId('random-repo-list')
+        .findAllByTestId('repo-card')
+        .should('have.length', 1);
+      cy.findByTestId('random-repo-list')
+        .findByTestId('name')
+        .invoke('text')
+        .should('be.oneOf', names);
+      cy.findByLabelText('Random Pick Button').click();
+      cy.findByTestId('random-repo-list')
+        .findAllByTestId('repo-card')
+        .should('have.length', 1);
+      cy.findByTestId('random-repo-list')
+        .findByTestId('name')
+        .invoke('text')
+        .should('be.oneOf', names);
+    });
+  });
+
+  it('reload should update the picked item', () => {
+    cy.fetchReposAndWait();
+
+    cy.fixture('trending').then(json => {
+      const names = json.map(repo => repo.name);
+      cy.findByTestId('random-repo-list')
+        .findByTestId('name')
+        .invoke('text')
+        .should('be.oneOf', names);
+    });
+
+    cy.fixture('trending-2').then(json => {
+      const names = json.map(repo => repo.name);
+      cy.route({
+        method: 'GET',
+        url: 'https://github-trending-api.now.sh/repositories*',
+        response: 'fixture:trending-2',
+      }).as('fetchRepos');
+      cy.findByTestId('last-updated-time').click();
+      cy.waitResponse();
+      cy.findByTestId('random-repo-list')
+        .findByTestId('name')
+        .invoke('text')
+        .should('be.oneOf', names);
+    });
   });
 });
